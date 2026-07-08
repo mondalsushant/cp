d808105f5936726c0f57066d90818dd775c3b971
Sushant Mondal <contact@sushantmondal.com>
2026-07-08 22:54:10 +0530

    feat(learncpp.com): add solution for 0.7

---
+++ solutions/learncpp.com/0.7.cpp
@@ -0,0 +1,14 @@
+// SPDX-FileCopyrightText: © 2026 Sushant Mondal <contact@sushantmondal.com>
+//
+// SPDX-License-Identifier: MIT
+//
+// Refs: https://www.learncpp.com/cpp-tutorial/compiling-your-first-program/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "Hello, world!\n";
+  return 0;
+}
